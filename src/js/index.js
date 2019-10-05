9a8e18e39a2ba4dd59613e5e8f789a733b75954b
John Waggener <john.waggener@gmail.com>
2019-10-04 20:57:19 -0400

    move styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import Yankees from "./yankees";
-import "../sass/app.scss";
+import "../styles/app.scss";
 import "../../node_modules/normalize.css/normalize.css";
 
 render(<Yankees />, document.getElementById("root"));
